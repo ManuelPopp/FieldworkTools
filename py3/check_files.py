649f767c5218dac8651fb15209ccc46b3f71275c
ManuelPopp <geooekologie@cdpopp.de>
2025-11-06 18:55:26 -0500

    Bug fix

--- py3/check_files.py
+++ py3/check_files.py
@@ -30,12 +30,11 @@ folders = [
 
 data = {
     folder: {
-        f"{sub}/{fname.format(PLOT = folder)}" if sub else fname.format(
+        f"{sub}/{fname.format(PLOT = folder)}" if sub != "" else fname.format(
             PLOT = folder
-            ):
-            os.path.exists(
-                os.path.join(
-                    base_dir, folder, sub, fname.format(PLOT = folder)
+        ): os.path.exists(
+            os.path.join(
+                base_dir, folder, sub, fname.format(PLOT = folder)
                     )
                 ) for sub, fnames in files_to_check.items() for fname in fnames
     }
