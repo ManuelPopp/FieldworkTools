099f92ca7b37dedc191adc01cb41d6d9c6b2d0aa
ManuelPopp <geooekologie@cdpopp.de>
2025-11-06 20:22:50 -0500

    Bug fix

--- py3/check_files.py
+++ py3/check_files.py
@@ -3,39 +3,33 @@ import pandas as pd
 from openpyxl import load_workbook
 from openpyxl.styles import PatternFill
 
-base_dir = "F:/FIELDWORK/"
+base_dir = "F:/FIELDWORK"
 files_to_check = {
     "": [
         "{PLOT}.gpx", "{PLOT}_points.gpkg", "{PLOT}_boundary.gpkg",
         "{PLOT}_L2.kmz", "{PLOT}_M3M.kmz",
         "{PLOT}_report_L2.kmz", "{PLOT}_report_M3M.kmz"
-        ],
-    "/Licor": [
-        "/Above/{PLOT}-A.txt",
-        "/Below/{PLOT}-B.txt",
+    ],
+    "Licor": [
+        "Above/{PLOT}-A.txt",
+        "Below/{PLOT}-B.txt",
         "{PLOT}_Processed_coords.xlsx"
-        ],
-    "/DJITerra": [
+    ],
+    "DJITerra": [
         "GNDVI.tif", "LCI.tif", "NDRE.tif", "NDVI.tif", "OSAVI.tif",
-        "result.tif",
-        "result_Green.tif", "result_NIR.tif",
+        "result.tif", "result_Green.tif", "result_NIR.tif",
         "result_RedEdge.tif", "result_Red.tif",
         "cloud_merged.las", "dem.tif", "dom.tif", "dsm.tif"
     ]
 }
 
-folders = [
-    f for f in os.listdir(base_dir) if os.path.isdir(os.path.join(base_dir, f))
-    ]
+folders = [f for f in os.listdir(base_dir) if os.path.isdir(os.path.join(base_dir, f))]
 
 data = {
     folder: {
-        f"{sub}{fname.format(PLOT = folder)}":
-        os.path.exists(
-            "/".join([
-                base_dir, folder, f"{sub}{fname.format(PLOT = folder)}"
-                    ])
-                ) for sub, fnames in files_to_check.items() for fname in fnames
+        os.path.join(sub, fname.format(PLOT = folder)): 
+        os.path.exists(os.path.join(base_dir, folder, sub, fname.format(PLOT = folder)))
+        for sub, fnames in files_to_check.items() for fname in fnames
     }
     for folder in folders
 }
@@ -46,16 +40,12 @@ df.to_excel(excel_path)
 
 wb = load_workbook(excel_path)
 ws = wb.active
-green = PatternFill(
-    start_color = "C6EFCE", end_color = "C6EFCE", fill_type = "solid"
-    )
-red = PatternFill(
-    start_color = "FFC7CE", end_color = "FFC7CE", fill_type = "solid"
-    )
+green = PatternFill(start_color = "C6EFCE", end_color = "C6EFCE", fill_type = "solid")
+red = PatternFill(start_color = "FFC7CE", end_color = "FFC7CE", fill_type = "solid")
 
 for r in range(2, ws.max_row + 1):
     for c in range(2, ws.max_column + 1):
-        cell = ws.cell(row=r, column=c)
+        cell = ws.cell(row = r, column = c)
         if cell.value is True:
             cell.fill = green
         elif cell.value is False:
